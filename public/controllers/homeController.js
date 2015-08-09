77d5a5668160d0e08769c25ac2693c3fbe1aa711
Jayadev <jaidev.akkiraju@gmail.com>
2015-08-08 23:25:36 -0700

    added current city service to get the lat long

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -1,7 +1,39 @@
-weatherApp.controller('HomeCtrl',['$scope','$resource','stateService','cityService','WeatherService',function($scope,$resource,stateService,cityService,WeatherService){
+weatherApp.controller('HomeCtrl',['$scope','$resource','stateService','cityService','WeatherService','currentCityService',function($scope,$resource,stateService,cityService,WeatherService,currentCityService){
 	stateService.getStates().then(function(data){
 		$scope.states=data;
 	});
+	
+	currentCityService.getCity().then(function(data){
+		console.log(data);
+		geocoder = new google.maps.Geocoder();
+		var latlng = new google.maps.LatLng(data.latitude, data.longitude);
+		
+		geocoder.geocode(
+		    {'latLng': latlng}, 
+		    function(results, status) {
+		        if (status == google.maps.GeocoderStatus.OK) {
+		                if (results[0]) {
+		                    var add= results[0].formatted_address ;
+		                    var  value=add.split(",");
+		
+		                    count=value.length;
+		                    country=value[count-1];
+		                    state=value[count-2];
+		                    city=value[count-3];
+		                    alert("city name is: " + city);
+		                }
+		                else  {
+		                    alert("address not found");
+		                }
+		        }
+		         else {
+		            alert("Geocoder failed due to: " + status);
+		        }
+		    }
+		);
+		
+	});
+	
 	
 	$scope.selectState=function(state){
 		$scope.state=state;
@@ -32,19 +64,19 @@ weatherApp.controller('HomeCtrl',['$scope','$resource','stateService','cityServi
 	};
 	
 	$(document).ready(function(){
-		alert(geoplugin_city());
+		// alert(geoplugin_city());
 	});
 	
-	$(document).on('mouseenter','#location',function(){
+	$(document).on('mouseenter','#query',function(){
 		$('#weather_get').css({ "z-index": "1110",'display':'block'});
 	});
 	
-	$(document).on('mouseenter','#weather_get',function(){
-		$('#weather_get').css('display','block');
-	});
+	// $(document).on('mouseenter','#weather_get',function(){
+		// $('#weather_get').css('display','block');
+	// });
 	
-	$(document).on('mouseleave','#location',function(){
-		$('#weather_get').css({ "z-index": "1000",'display':'block'});
+	$(document).on('mouseleave','#query',function(){
+		$('#weather_get').css({ 'display':'none'});
 	});
 	 
 }]);
---
+++ public/services/currentCityService.js
@@ -0,0 +1,26 @@
+weatherApp.factory('currentCityService',['$http','$q',function($http,$q){
+	var currentCity={};
+	
+	currentCity.getCity=function(){
+		var deferred = $q.defer();
+		    if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(function(position){
+		    		console.log(position);
+				    console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+				     
+				    var coordinates={
+				    	"latitude":	position.coords.latitude,
+				    	"longitude": position.coords.longitude
+				    };
+				    // console.log(coordinates);
+				    deferred.resolve(coordinates);
+		        });
+		    } else { 
+		    	deferred.reject("Geolocation is not supported by this browser.");
+		        console.log("Geolocation is not supported by this browser.");
+		    }
+		    return deferred.promise;
+	};
+	
+	return currentCity;
+}]);
